e27f4c2be0abc3f1fb444f064db6ef3060a8a3a6
magomagoz <magostinienrico@gmail.com>
2026-03-03 14:49:13 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -74,6 +74,16 @@ with st.sidebar:
         if st.button("🔴 SCOLLEGA"):
             st.session_state.connected = False
             st.rerun()
+        
+    st.divider()
+
+    # --- SESSIONI DI MERCATO ---
+    now_cet = datetime.now().time()
+    st.subheader("🌍 SESSIONI DI MERCATO")
+        
+    for city, (start, end) in {"LONDRA 🇬🇧": (time(9,0), time(18,0)), "NEW YORK 🇺🇸": (time(14,0), time(23,0)), "SYDNEY 🇦🇺": (time(23,0), time(8,0)), "TOKYO 🇯🇵": (time(1,0), time(10,0))}.items():
+        status = "🟢 Open: " if start <= now_cet <= end else "🔴 Closed: "
+        st.write(f"{status} {city}")
 
 # --- MAIN DASHBOARD ---
 if st.session_state.connected:
@@ -90,14 +100,6 @@ if st.session_state.connected:
         
     st.divider()
         
-    # --- SESSIONI DI MERCATO ---
-    now_cet = datetime.now().time()
-    st.subheader("🌍 SESSIONI DI MERCATO")
-        
-    for city, (start, end) in {"LONDRA 🇬🇧": (time(9,0), time(18,0)), "NEW YORK 🇺🇸": (time(14,0), time(23,0)), "SYDNEY 🇦🇺": (time(23,0), time(8,0)), "TOKYO 🇯🇵": (time(1,0), time(10,0))}.items():
-        status = "🟢 Open: " if start <= now_cet <= end else "🔴 Closed: "
-        st.write(f"{status} {city}")
-
 # --- MAIN DASHBOARD ---
 if st.session_state.connected:
     Iq = st.session_state.iq
