4e545ef9252b84cd627957cad953487fcd3a64da
magomagoz <magostinienrico@gmail.com>
2026-03-05 12:09:24 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -93,6 +93,9 @@ with st.sidebar:
                 st.rerun()
     else:
         st.success(f"🟢 Conto {st.session_state.account_type} ATTIVO")
+        
+        st.metric(f"💰 Saldo {st.session_state.account_type}", f"{st.session_state.get_balance:.2f} €")    
+        
         st.session_state.stake = st.number_input("💰 INVESTIMENTO (€)", value=100.0)
         
         timeframe = st.selectbox("⏱️ SELEZIONA TIMEFRAME OPERATIVO (s)", [60, 300], index=0)
