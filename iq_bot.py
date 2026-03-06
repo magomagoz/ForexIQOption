3c1246699b68cec84c84e716af63164de387e760
magomagoz <magostinienrico@gmail.com>
2026-03-06 09:59:40 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -131,7 +131,7 @@ def draw_market_map_inverted(current_hour_float, trading_autorizzato):
 
     # --- LOGICA RITARDO (OFFSET) ---
     # Sottraiamo 30 minuti
-    ritardo_ore = 23 / 60
+    ritardo_ore = 22 / 60
     x_pos = current_hour_float - ritardo_ore
 
     # Gestione del reset: se l'ora è 00:20, sottraendo 40 min andrebbe in negativo.
