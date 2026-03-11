cfd1a143d60fd89b00921b6794e751a7f22bd376
magomagoz <magostinienrico@gmail.com>
2026-03-11 23:04:53 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -394,7 +394,7 @@ if st.session_state.connected:
         
         fig.add_trace(go.Scatter(x=df_final.index, y=df_final['RSI'], line=dict(color='#AB63FA'), name="RSI"), row=2, col=1)
         fig.add_hline(y=(45 if stress_test else 28), line_color="green", row=2, col=1, line_dash="dash")
-	        fig.add_hline(y=(55 if stress_test else 72), line_color="red", row=2, col=1, line_dash="dash")
+	    fig.add_hline(y=(55 if stress_test else 72), line_color="red", row=2, col=1, line_dash="dash")
 	
 	        macd_colors = []
 	        hist_diff = df_final['HIST'].diff()
