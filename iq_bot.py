04c36d302f729197a0ed6f28356c9ce14d5ca58e
magomagoz <magostinienrico@gmail.com>
2026-03-12 11:24:53 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -401,9 +401,9 @@ if st.session_state.connected:
             fig.add_trace(go.Candlestick(x=df_final.index, open=df_final['open'], high=df_final['max'], 
                                          low=df_final['min'], close=df_final['close'], name="Prezzo"), row=1, col=1)
             
-            fig.add_trace(go.Scatter(x=df_final.index, y=df_final['BBU'], line=dict(color='rgba(0,71,171,0.4)'), name="BBU"), row=1, col=1)
+            fig.add_trace(go.Scatter(x=df_final.index, y=df_final['BBU'], line=dict(color='rgba(0,71,171,0.2)'), name="BBU"), row=1, col=1)
             fig.add_trace(go.Scatter(x=df_final.index, y=df_final['BBM'], line=dict(color='rgba(170,170,170,0.3)', width=1), name="BBM"), row=1, col=1)
-            fig.add_trace(go.Scatter(x=df_final.index, y=df_final['BBL'], line=dict(color='rgba(0,71,171,0.4)'), 
+            fig.add_trace(go.Scatter(x=df_final.index, y=df_final['BBL'], line=dict(color='rgba(0,71,171,0.2)'), 
                                      fill='tonexty', fillcolor='rgba(100, 100, 255, 0.05)', name="BBL"), row=1, col=1)
             
             # Subplot 2: RSI con soglie dinamiche
