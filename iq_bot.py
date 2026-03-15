0d8eee56536dc8339636fa5062032bb92f308e49
magomagoz <magostinienrico@gmail.com>
2026-03-15 12:53:56 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -248,8 +248,6 @@ with st.sidebar:
             
         st.session_state.weekend_mode = st.toggle("🧠🍹 ATTIVA SABATO MAGICO", value=st.session_state.weekend_mode)
 
-        st.divider()
-        
         if st.session_state.weekend_mode:
             # 1. COSA SUCCEDE SE È ACCESA:
             st.success("🎯 **Sniper Attivo:** \n\n**RSI (20/80)** - **BB (20, 2.5)** - **no MACD**")
@@ -259,7 +257,9 @@ with st.sidebar:
             bb_period, bb_std = 20, 2.50
             custom_rsi_buy, custom_rsi_sell = 20, 80
             custom_macd_fast, custom_macd_slow, custom_macd_sig = 12, 26, 9 # Ininfluenti perché use_macd è False
-            
+
+        st.divider()
+        
         else:
             # 2. COSA SUCCEDE SE È SPENTA: (Appare il tuo setup classico)
             st.subheader("🎛️ Setup Indicatori (Live)")
