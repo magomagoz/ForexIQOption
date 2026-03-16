0c345e7f34bd8ae6117cd8e5a1797ed264927728
magomagoz <magostinienrico@gmail.com>
2026-03-16 22:48:11 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -259,10 +259,10 @@ with st.sidebar:
             
             c_bb1, c_rsi1 = st.columns(2)
             bb_period = c_bb1.selectbox("Periodo BB", [20, 14], index = 0)
-            custom_rsi_buy = c_rsi1.selectbox("RSI Buy", [30, 28, 25], index = 1)
+            custom_rsi_buy = c_rsi1.selectbox("RSI Buy", [30, 28, 25], index = 0)
                         
             c_bb2, c_rsi2 = st.columns(2)
-            bb_std = c_bb2.selectbox("Dev BB", [1.80, 2.00, 2.20], index = 2)
+            bb_std = c_bb2.selectbox("Dev BB", [1.80, 2.00, 2.20], index = 1)
             custom_rsi_sell = c_rsi2.selectbox("RSI Sell", [70, 72, 75], index = 1)
 
         st.divider()
