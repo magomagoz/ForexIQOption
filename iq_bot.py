ad6d96abf88c22b49e1a3ca30a0a9966914d66c2
magomagoz <magostinienrico@gmail.com>
2026-03-18 21:54:50 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -759,7 +759,7 @@ if st.session_state.connected:
         time_end = st.time_input("🛑 Orario Fine:", value=time(23, 59))
 
     #df_journal['ora_attuale'] = pd.to_datetime(df_journal['time'], errors='coerce').dt.time
-    df_filtered = df_journal[(df_journal['ora_attuale'] >= time_start) & (df_journal['ora_attuale'] <= time_end)].copy()
+    #df_filtered = df_journal[(df_journal['ora_attuale'] >= time_start) & (df_journal['ora_attuale'] <= time_end)].copy()
         
     if filtro_mercato != "TUTTI":
         df_filtered = df_filtered[df_filtered['mercato'].str.contains(filtro_mercato, na=False)]
