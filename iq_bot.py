ec93a3a2aa6fc55ee70742ebfd4f90c7c9e5ee14
magomagoz <magostinienrico@gmail.com>
2026-03-18 19:07:33 +0100

    Update iq_bot.py

--- iq_bot.py
+++ iq_bot.py
@@ -397,18 +397,18 @@ if st.session_state.connected:
 
     st.subheader("🌍 Live Market Flow 24h")
     
-    if st.session_state.weekend_mode or is_weekend_reale:
-        try:
+    #if st.session_state.weekend_mode or is_weekend_reale:
+        #try:
             # Carica banner2.png se siamo in modalità weekend
-            img_weekend = Image.open("banner2.png")
-            st.image(img_weekend, use_column_width=True, caption="MODALITÀ WEEKEND ATTIVA 🔴 MERCATI CHIUSI")
-        except:
-            st.warning("Immagine banner2.png non trovata. Carica il file nella cartella del progetto.")
-    else:
-        st.plotly_chart(draw_market_map_inverted(trading_autorizzato), use_container_width=True)
+            #img_weekend = Image.open("banner2.png")
+            #st.image(img_weekend, use_column_width=True, caption="MODALITÀ WEEKEND ATTIVA 🔴 MERCATI CHIUSI")
+        #except:
+            #st.warning("Immagine banner2.png non trovata. Carica il file nella cartella del progetto.")
+    #else:
+        #st.plotly_chart(draw_market_map_inverted(trading_autorizzato), use_container_width=True)
 
         # --- GESTIONE STATO SCANNER E PROTEZIONE ORARIA ---
-        #esegui_scansione = False # Di default è spento
+        esegui_scansione = False # Di default è spento
         
         if st.session_state.scanner_on:
             # Messaggio dinamico in base alla modalità
